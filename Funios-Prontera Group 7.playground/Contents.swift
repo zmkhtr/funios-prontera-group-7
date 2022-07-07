65b28c27d4ce71812e7ad396b55c423bb4699748
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-07-07 20:46:23 +0700

    Pengenalan type data

--- Funios-Prontera Group 7.playground/Contents.swift
+++ Funios-Prontera Group 7.playground/Contents.swift
@@ -10,4 +10,10 @@ let kotakTiga: String = "20" // ini variabel dengan type data
 
 print("Isi kotak tiga adalah \(kotakTiga)")
 
+var umur: Int = 12
+let name: String = "Azam"
+var tinggiBadan: Double = 12.0
+let sudahLulus: Bool = true
+let inisial: Character = "A"
+
 
